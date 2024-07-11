be21b694388da788db72680f7f9183230d58fbd7
Mulih <almulih@yahoo.com>
2024-07-11 22:08:49 +0000

    upload of rest of the files

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,6 +26,26 @@ def unauthorized_error(error) -> str:
     '''unauthorized handler'''
     return jsonify({"error": "Unauthorized"}), 401
 
+@app.before_request
+def before_request() -> str:
+    '''Request Handler
+    requests Validation'''
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
