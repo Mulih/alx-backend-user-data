726afa9a41b64a6583a15f74e7621f0c827b8ca1
Mulih <almulih@yahoo.com>
2024-07-18 22:51:41 +0000

    Hot Fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
+    """Represents user object."""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
