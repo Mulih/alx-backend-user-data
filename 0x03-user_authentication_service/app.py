0362b8496d0baaa7732e236520f18b1963695499
Mulih <almulih@yahoo.com>
2024-07-19 00:36:57 +0000

    implement user route in app.pt

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -14,6 +16,16 @@ def home() -> str:
     """
 
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/user", methods["POST"])
+def users():
+    email = request.form.get("email")
+    password = requestt.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
